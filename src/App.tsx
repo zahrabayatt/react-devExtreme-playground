eac284d2d135e8cc4f8810112299d0af063c5e1d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-02 18:03:25 +0330

    Add List component sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "devextreme/dist/css/dx.light.css";
+import "./App.css";
+import { List } from "devextreme-react/list";
+import { products } from "./data/product";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List
+        dataSource={products}
+        displayExpr={(d) => `${d.Name} - ${d.Category}`}
+      ></List>
     </div>
   );
 }
