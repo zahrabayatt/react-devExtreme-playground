03525829263e2071c6fc61172d0a35f5344b919f
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-07 20:56:13 +0330

    Add HoverTagPopover component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Popover } from "devextreme-react/popover";
 import "devextreme/dist/css/dx.light.css";
-import PopulationChart from "./components/PopulationChart";
+import { products } from "./data/product";
+import { TagBox } from "devextreme-react";
+import "./App.css";
+import HoverTagPopover from "./components/HoverTagPopover";
 
 function App() {
-  return <PopulationChart />;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "3px" }}>
+      <HoverTagPopover title="Hello" id="1" />
+      <HoverTagPopover title="Hello 2" id="2" />
+      <HoverTagPopover title="Hello 3" id="3" />
+      <HoverTagPopover title="Hello 4" id="4" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HoverTagPopover.tsx
@@ -0,0 +1,27 @@
+import { Popover } from "devextreme-react";
+import { title } from "process";
+import React, { ReactNode } from "react";
+
+interface Props {
+  id: string;
+  title: string;
+  popoverContent?: ReactNode;
+}
+
+const HoverTagPopover = ({ id, title, popoverContent }: Props) => {
+  return (
+    <>
+      <div id={`tag-${id}`} className="tag-text">
+        {title}
+      </div>
+      <Popover
+        target={`#tag-${id}`}
+        showEvent="dxhoverstart"
+        hideEvent="dxhoverend"
+        contentRender={() => <p>Popover content</p>}
+      />
+    </>
+  );
+};
+
+export default HoverTagPopover;
